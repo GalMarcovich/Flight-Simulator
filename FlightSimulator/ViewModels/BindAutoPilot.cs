11a8d35595118d60f099be92a65b2ed838c60bb6
Gal Marcovich <gal.marcovich@live.biu.ac.il>
2019-04-14 19:52:43 +0300

    info and SettingsWindowViewModel

---
+++ FlightSimulator/ViewModels/BindAutoPilot.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightSimulator.Model;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FlightSimulator.ViewModels
+{
+    class BindAutoPilot : BaseNotify
+    {
+        string[] input;
+        string line;
+        public BindAutoPilot()
+        {
+            line = "";
+        }
+
+        public string Line
+        {
+            get
+            {
+                // change color if needed
+                NotifyPropertyChanged("Color");
+                return line;
+            }
+            set
+            {
+                line = value;
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                if (line == "")
+                {
+                    return "White";
+                }
+                else
+                {
+                    return "Pink";
+                }
+            }
+            private set { }
+        }
+        private ICommand _clickCommand;
+        public ICommand ClickCommand
+        {
+            get
+            {
+                return _clickCommand ?? (_clickCommand = new CommandHandler(() => OnClick()));
+            }
+        }
+        private void OnClick()
+        {
+            Parse(line);
+
+            foreach (string arg in input)
+            {
+                MessageBox.Show(arg);
+            }
+        }
+
+        private ICommand _clearCommand;
+        public ICommand ClearCommand
+        {
+            get
+            {
+                return _clearCommand ?? (_clearCommand = new CommandHandler(() => ClearClick()));
+            }
+        }
+        private void ClearClick()
+        {
+            line = "";
+            NotifyPropertyChanged(line);
+        }
+
+        private void Parse(string line)
+        {
+            string[] newLine = { "\r\n" };
+            input = line.Split(newLine, StringSplitOptions.None);
+        }
+    }
+}
--- FlightSimulator/Views/Settings.xaml.cs
+++ FlightSimulator/Views/Settings.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using FlightSimulator.Model;
+using FlightSimulator.ViewModels.Windows;
 
 namespace FlightSimulator.Views
 {
@@ -22,6 +24,7 @@ namespace FlightSimulator.Views
         public Settings()
         {
             InitializeComponent();
+            this.DataContext = new SettingsWindowViewModel(ApplicationSettingsModel.Instance);
         }
     }
 }
